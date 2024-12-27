41f057c648e60c173043fe52169e42d33e5e220a
Ildar Bikmamatov <ildar@bayrell.org>
2024-12-28 02:43:03 +0600

    check if BayLang Constructor is installed

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 /* Check if Wordpress */
 if (!defined('ABSPATH')) exit;
 
+/* Check if BayLang Constructor is installed */
+if (!class_exists(\Runtime\rtl::class))
+{
+	status_header(500);
+	echo "Install BayLang Constructor";
+}
+
 /* Run web app */
-$context = \Runtime\rtl::getContext();
-$context->run($context);
+else
+{
+	$context = \Runtime\rtl::getContext();
+	$context->run($context);
+}
